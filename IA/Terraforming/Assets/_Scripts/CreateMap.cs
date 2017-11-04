94d82869732d2500c866d76d8857709f2382c8f4
Unknown <leo11leo55@yahoo.com>
2017-11-04 22:52:55 +0000

    Terrain
    No more cubes

--- IA/Terraforming/Assets/_Scripts/CreateMap.cs
+++ IA/Terraforming/Assets/_Scripts/CreateMap.cs
@@ -6,42 +6,84 @@ using UnityEngine.UI;
 public class CreateMap : MonoBehaviour {
 
     int X, Y;
+    public Terrain terreno;
     public Canvas CanvasStart;
     public Text Xtext, Ytext;
     public Slider Xslider, Yslider;
     public GameObject prefab;
+    float[,] heights;
+
+    float random;
+
+    float scale = 20f;
 
 	void Start ()
     {
-        
-	}
+        GetValeus();
+    }
 		
 
 	void Update ()
     {
-        Xtext.text = "" + Xslider.value;
 
-        Ytext.text = "" + Yslider.value;
-    }
 
-    void DrawBase()
-    {
-        for (int i = 0; i < X; i++)
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            for (int j = 0; j < Y; j++)
-            {
-                Instantiate(prefab, new Vector3(i, -0.5F, j), Quaternion.identity);
-            }
+            HeightRandom();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ResetHeight();
         }
     }
 
     public void GetValeus()
     {
-        X = (int)Xslider.value;
-        Y = (int)Yslider.value;
+        X = 32;
+        Y = 32;
+        terreno.terrainData.heightmapResolution = X + 1;
+        terreno.terrainData.size = new Vector3(X, scale, Y);
+        heights = new float[X,Y];
 
-        CanvasStart.gameObject.SetActive(false);
+        // Alterar locais do terreno!!!
+        terreno.terrainData.SetHeights(0, 0, heights);
+        
+    }
 
-        DrawBase();
+    void HeightRandom()
+    {
+        for (int i = 0; i < X; i++)
+        {
+            for (int j = 0; j < Y; j++)
+            {
+                Debug.Log("Alterando as alturas!");
+                random = Random.Range(0.0f, 1.0f);
+                heights[i,j] = CalculateHeight(i, j);
+            }
+        }
+        terreno.terrainData.SetHeights(0, 0, heights);
+    }
+
+    
+    float CalculateHeight(int x, int y)
+    {
+        float xCoord = (float)x / X * scale;
+        float yCoord = (float)y / Y * scale;
+
+        return Mathf.PerlinNoise(xCoord, yCoord);
+    }
+
+    void ResetHeight()
+    {
+        for (int i = 0; i < X; i++)
+        {
+            for (int j = 0; j < Y; j++)
+            {
+                Debug.Log("Reset das alturas!");
+                random = Random.Range(0.0f, 1.0f);
+                heights[i, j] = 0.0f;
+            }
+        }
+        terreno.terrainData.SetHeights(0, 0, heights);
     }
 }
