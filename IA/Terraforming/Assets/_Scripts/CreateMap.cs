58fdf36f62c536e9590e45c3a4db7d8b54914f57
Unknown <leo11leo55@yahoo.com>
2017-11-04 15:59:04 +0000

    base_map

---
+++ IA/Terraforming/Assets/_Scripts/CreateMap.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateMap : MonoBehaviour {
+
+    int X, Y;
+    public Canvas CanvasStart;
+    public Text Xtext, Ytext;
+    public Slider Xslider, Yslider;
+    public GameObject prefab;
+
+	void Start ()
+    {
+        
+	}
+		
+
+	void Update ()
+    {
+        Xtext.text = "" + Xslider.value;
+
+        Ytext.text = "" + Yslider.value;
+    }
+
+    void DrawBase()
+    {
+        for (int i = 0; i < X; i++)
+        {
+            for (int j = 0; j < Y; j++)
+            {
+                Instantiate(prefab, new Vector3(i, -0.5F, j), Quaternion.identity);
+            }
+        }
+    }
+
+    public void GetValeus()
+    {
+        X = (int)Xslider.value;
+        Y = (int)Yslider.value;
+
+        CanvasStart.gameObject.SetActive(false);
+
+        DrawBase();
+    }
+}
